986f819d7e53b2f8c687eaf7eb3aa9ceda6ae304
AneelaTasneem <aneelatasneem90@gmail.com>
2024-03-19 16:55:22 -0700

    Changes made

---
+++ 52_Make_a_smartphone_object/q52.js
@@ -0,0 +1,15 @@
+"use strict";
+// Make a smartphone object:
+// Settin up details about smartphone:
+let mySmartPhone = {
+    brand: "Android",
+    model: "ONEPlUSA6010",
+    storage: "128 GB",
+    screenSize: "6.41 inches",
+    batteryLife: "10 Hours",
+};
+// Accessing the smartphone's details
+console.log(`My phone is a ${mySmartPhone.brand} ${mySmartPhone.model}.`);
+console.log(`It has ${mySmartPhone.storage} of storage.`);
+console.log(`The screen size is ${mySmartPhone.screenSize} .`);
+console.log(`The battery lasts for ${mySmartPhone.batteryLife} .`);
