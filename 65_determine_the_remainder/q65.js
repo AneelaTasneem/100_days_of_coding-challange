93dca4ef9494096970f7c54e345ba090d7bca458
AneelaTasneem <aneelatasneem90@gmail.com>
2024-03-24 09:46:26 -0700

    day 22 challage

---
+++ 65_determine_the_remainder/q65.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question 65: Determine the Remainder: Make a function that gets two numbers and shows the leftover from dividing them using the % sign. For example, remainder(5, 2) should give 1.
+//This function will divide the values to find remainder:
+function determineTheRemainder(number1, number2) {
+    // give back the leftover by dividing values:
+    return number1 % number2;
+}
+// 5 divided by two:
+console.log(determineTheRemainder(5, 2));
---
+++ 66_logical_and_verifications/q66.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question 66: Logical AND Verification: Create a function that checks two boolean (true or false) values. It should only say true if both are true, using the && operator. For instance, checkBothTrue(true, false) should be false.
+// This function sees if both inputs are true
+function checkBothTrue(value1, value2) {
+    // Only says true if both val1 and val2 are true
+    return value1 && value2;
+}
+// Trying it with true and false
+console.log(checkBothTrue(true, false));
