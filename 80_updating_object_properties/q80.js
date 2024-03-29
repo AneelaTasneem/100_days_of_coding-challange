96323332a1ccd9dd9f7ffbbf0efd6675cfb043b1
AneelaTasneem <aneelatasneem90@gmail.com>
2024-03-28 18:51:58 -0700

    day 27 assignment

---
+++ 80_updating_object_properties/q80.js
@@ -0,0 +1,15 @@
+"use strict";
+// Question 80: Updating Object Properties: Add a property named color to the existing car object, and then update the year property to 2021. Show how to perform these operations.
+// Define a car object
+let car = {
+    make: "Honda",
+    model: "Civic",
+    year: "2020", // Initial year
+    color: "", // Define color property with an initial value
+};
+// Add a color property to the car
+car.color = "Black";
+// Update the year of the car
+car.year = "2021";
+// Print the car object
+console.log(car);
